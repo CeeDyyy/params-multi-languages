c245cb54308750aaf8847a2468a0ac22f3ab13b1
CeeDyyy <thanawat.ud@mail.kmutt.ac.th>
2023-03-20 15:45:15 +0700

    nuxt-composition

---
+++ nuxt-composition/pages/[lng].vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ language.heading }}</h1>
+    <p>{{ language.content }}</p>
+    <div>
+      {{ language.language }}: <b>{{ $route.params.lng }}</b>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import English from "/languages/english.json";
+import Thai from "/languages/thai.json";
+import Chinese from "/languages/chinese.json";
+import Japanese from "/languages/japanese.json";
+
+const language = ref(English);
+const route = useRoute();
+
+onMounted(() => {
+  if (route.params.lng == "en") {
+    language.value = English;
+  }
+  if (route.params.lng == "th") {
+    language.value = Thai;
+  }
+  if (route.params.lng == "cn") {
+    language.value = Chinese;
+  }
+  if (route.params.lng == "jp") {
+    language.value = Japanese;
+  }
+});
+</script>
---
+++ nuxt-composition/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>loading...</div>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    location.href = "/en";
+});
+</script>
